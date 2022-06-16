97e3be2a7ee76ee566990ca0fb179e5cf7bc19ca
Rock Ponn <ratannak.ponn@outlook.com>
2022-06-15 19:44:52 -0500

    algos

--- Easy/stringReversal.js
+++ Easy/stringReversal.js
@@ -1,1 +1,12 @@
 /* Reverse the string without using reverse method */
+
+
+function revStr(str) {
+    
+    let newStr = "";
+    for(let i = str.length - 1; i >= 0; i--) {
+        newStr+= str[i]
+    }
+
+    return newStr;
+}
